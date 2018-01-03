6b68791a24f90e71a36d2114abe1fdeebfd84aee
Kimmo Tuokkola <kimmo.tuokkola@accenture.com>
2018-01-03 13:43:15 +0200

    ..

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(window).load(function() {
+  
+  $('ul.members').find('a').each(function() {
+    var $this = $(this);
+    var id = $('h1.family-tree-title').data('id');
+    if ($this.data('id') === id) {
+      $this.parent().addClass('active2');
+    }      
+  });
 });
